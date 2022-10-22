d7d0318e74183bd99af6c94c4c2188cde6898f19
Oluwatomilola Adeniran <84162702+tun-ji@users.noreply.github.com>
2022-10-22 14:38:04 +0100

    Time: 70 ms (96.23%), Space: 43.1 MB (33.75%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var map = {};
+    
+    for (let i = 0 ; i < nums.length; i++) {
+        if (target - nums[i] in map) {
+            return [i, map[target - nums[i]]]
+        } else {
+            map[nums[i]] = i  
+        }
+    }
+
+}
